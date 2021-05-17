a9bc8edd307878709b911df322d90f0c40602e14
keitaabdou <abdou.keita@uvci.edu.ci>
2021-05-17 10:15:44 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './components/index.css';
 import reportWebVitals from './reportWebVitals';
+import Game from './components/Game';
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
